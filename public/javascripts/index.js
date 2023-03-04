06a5b19ed2f610fe04216a61fc8d294389d425d2
siam1113 <hasan.2017.siam@gmail.com>
2023-03-04 22:34:02 +0600

    Basic functionality of Cart page, Payment Page & Checkout page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,56 @@
 let addDrinkButtons = document.getElementsByClassName("add");
-let addedDrinks = {};
+let cart = document.getElementById("cart");
+let clearCart = document.getElementById("clear");
+let cartItemCount = document.getElementById("cart-count");
+let addedDrinks = JSON.parse(localStorage.getItem("cart"));
+addedDrinks = addedDrinks == null ? {} : addedDrinks;
+
+// Adding count in UI
+for (const drink in addedDrinks) {
+  cartItemCount.innerText =
+    Number(cartItemCount.innerText) + addedDrinks[drink]["count"];
+}
 
 function addDrink(e) {
-  let cartItemCount = document.getElementById("cart");
-  let productName = e.target.previousElementSibling.innerText;
+  let productName = e.target.previousElementSibling.firstChild.innerText;
+  let productPrice = Number(
+    e.target.previousElementSibling.lastChild.innerText.replace("$", "")
+  );
   addedDrinks[productName] =
-    addedDrinks[productName] == undefined ? 1 : addedDrinks[productName] + 1;
+    addedDrinks[productName] == undefined
+      ? { count: 1, price: productPrice }
+      : {
+          count: ++addedDrinks[productName]["count"],
+          price: addedDrinks[productName]["count"] * productPrice,
+        };
+  // Adding count in UI
   cartItemCount.innerText = Number(cartItemCount.innerText) + 1;
+  localStorage.setItem("cart", JSON.stringify(addedDrinks));
 }
+
 for (let addDrinkButton of addDrinkButtons) {
   addDrinkButton.addEventListener("click", addDrink);
 }
+
+clearCart.addEventListener("click", () => {
+  cartItemCount.innerText = 0;
+  addedDrinks = {};
+  localStorage.setItem("cart", JSON.stringify(addedDrinks));
+});
+
+// TO-UNDERSTAND
+cart.addEventListener("click", () => {
+  fetch("http://localhost:3000/cart/", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(addedDrinks), // body data type must match "Content-Type" header
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,53 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
+let addedDrinks = {};
+
+router.post("/cart", function (req, res, next) {
+  addedDrinks = req.body;
+});
+
 router.get("/cart", function (req, res, next) {
-  res.render("cart");
+  let totalPrice = 0;
+  for (drinks in addedDrinks) {
+    totalPrice += Number(addedDrinks[drinks]["price"]);
+  }
+  res.render("cart", { addedDrinks, totalPrice });
+});
+
+router.get("/payment", function (req, res, next) {
+  res.render("payment");
+});
+
+router.post("/checkout", function (req, res, next) {
+  if (req.body.fullName == undefined && req.body.name == undefined) {
+    res.render("payment", {
+      message: "Put your full name and a valid phone number",
+    });
+  } else if (
+    req.body.fullName == "" ||
+    req.body.fullName.length < 5 ||
+    req.body.phone.length < 11
+  ) {
+    console.log(req.body.fullName.length);
+    console.log(req.body.phone.length);
+    res.render("payment", {
+      message: "Put your full name and a valid phone number",
+    });
+  } else {
+    dbConnection.query(
+      "INSERT INTO drinks.orders VALUES()",
+      (err, rows, fields) => {
+        if (err) throw err;
+        console.log("Order Placed");
+        console.log(rows);
+        id = rows.insertId;
+        res.render("checkout", {
+          message: `Order has been placed. \nYour order Id ${id}`,
+        });
+      }
+    );
+  }
 });
 
 module.exports = router;
